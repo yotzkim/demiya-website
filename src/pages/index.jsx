1c86086921ea82de20cd8b7aed01e04ccb8d7eb8
Yoto Kim <yongtokim1215@gmail.com>
2025-04-21 17:15:56 -0700

    Home link bug fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,137 +1,70 @@
-import Link from "next/link";
-import styles from "../../src/styles/orders.module.css";
-import Layout from "../components/layout.jsx";
-import Head from "next/head";
+import Link from 'next/link';
+import styles from '../styles/home.module.css';
+import Image from 'next/image';
+import Layout from '../components/layout.jsx';
+import Head from 'next/head';
 
-export default function Order() {
-  const locations = [
-    {
-      name: "EMERYVILLE",
-      address: "The Public Market 5959 Shellmound St, Emeryville, CA 94608",
-      mapSrc: `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=5959+Shellmound+St+%2C+Emeryville+%2C+CA+94608`,
-      orderLink:
-        "https://order.toasttab.com/online/demiya-emeryville-5959-shellmound-street",
+export default function Home() {
+  const announcements = [
+    {text: 'You can now reserve tables at the Cuperitno / De Anza Blvd location through: ', linkText: 'OpenTable', linkHref: 'https://www.opentable.com/restref/client/?restref=1412428&lang=en-US&ot_source=Restaurant%20website&corrid=2c03b971-05d1-47df-92a2-98eaf21cbd6b'},
+    { text: 'New location at ',
+      boldText: 'The Public Market in Emeryville',
+      subText: ' is open!'
     },
-    {
-      name: "SARATOGA AVE",
-      address: "375 Saratoga Ave G, San Jose, CA 95129",
-      mapSrc: `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=375+Saratoga+Ave+G%2C+San+Jose%2C+CA+95129`,
-      orderLink:
-        "https://order.toasttab.com/online/demiya-saratoga-ave-em-375-saratoga-ave-ste-g",
-      doordashLink: "https://www.doordash.com/store/demiya-san-jose-31449623",
-    },
-    {
-      name: "CUPERTINO",
-      address: "1145 S De Anza Blvd, San Jose, CA 95129",
-      mapSrc: `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=1145+S+De+Anza+Blvd+%2C+San+Jose%2C+CA+95129`,
-      orderLink:
-        "https://order.toasttab.com/online/demiya-de-anza-em-1145-south-de-anza-boulevard",
-      doordashLink: "https://www.doordash.com/store/demiya-san-jose-31449571/",
-      ubereatsLink:
-        "https://www.ubereats.com/store/demiya-de-anza-blvd/ArVjGu43T1yi8lU9regVYQ?diningMode=DELIVERY",
-    },
-    {
-      name: "DUBLIN",
-      address: "7477 Village Pkwy, Dublin, CA 94568",
-      mapSrc: `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=7477+Village+Pkwy+%2C+Dublin+%2C+CA+94568`,
-      orderLink:
-        "https://order.toasttab.com/online/demiya-dublin-em-7477-village-parkway",
-      doordashLink: "https://www.doordash.com/store/demiya-dublin-31449671",
-    },
-    {
-      name: "FREMONT",
-      address: "46551 Mission Blvd, Fremont, CA 94539 (inside 99 Ranch)",
-      mapSrc: `https://www.google.com/maps/embed/v1/place?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&q=46551+Mission+Blvd+%2C+Fremont+%2C+CA+94539`,
-      orderLink:
-        "https://order.toasttab.com/online/demiya-fremont-em-46551-mission-boulevard",
-      doordashLink:
-        "https://www.doordash.com/en-AU/store/demiya-fremont-31282911/",
-      ubereatsLink:
-        "https://www.ubereats.com/store/demiya-fremont-em/aqjfiPLdQoCy7NTnFVmA9g",
-    },
+
   ];
 
-  const emeryville = locations.find((l) => l.name === "EMERYVILLE");
-  const others = locations.filter((l) => l.name !== "EMERYVILLE");
 
   return (
     <Layout>
       <Head>
-        <title>Demiya Online Order</title>
+        <title>Demiya Website</title>
         <meta
           name="description"
-          content="Official website for Demiya restaurant. Order online here!"
-        ></meta>
+          content="Official website for Demiya restaurant. Order online and check hours here!"
+        />
         <meta
           name="keywords"
           content="Demiya, Japanese Restaurant, Japanese Curry, Katsu, Cupertino, Dublin, San Jose, Saratoga"
-        ></meta>
-        <link rel="icon" href="/images/logo.jpeg"></link>
+        />
+        <link rel="icon" href="/images/logo.jpeg" />
       </Head>
-
-      <div className={styles.centeredEmeryville}>
-        <div className={styles.location}>
-          <p className={styles.locationName}>
-            <span className={styles.newLabel}>NEW:</span>
-            {emeryville.name}
-          </p>
-          <Link href={emeryville.orderLink}>Online order</Link>
-          <p>{emeryville.address}</p>
-          <div className={styles.mapContainer}>
-            <iframe
-              src={emeryville.mapSrc}
-              frameBorder="0"
-              allowFullScreen
-              title={emeryville.name}
-            ></iframe>
-          </div>
+      <div className={styles.container}>
+        <div className={styles.announcementsBox}>
+          <ul className={styles.announcementList}>
+            {announcements.map((announcement, index) => (
+              <li className={styles.announcementItem} key={index}>
+              {announcement.text}
+              <span style={{fontWeight: 'bold'}}>
+              {announcement.boldText}
+              </span>
+              {announcement.subText}
+              {announcement.linkText && (
+                <Link href={announcement.linkHref} className={styles.announcementLink}>
+                  {announcement.linkText}
+                </Link>
+              )}
+            </li>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.homeText}>
+          <span>The most up-to-date information is available on Instagram at </span>
+          <Link className={styles.instagramLink} href="https://www.instagram.com/demiya_inc/">
+            demiya_inc
+          </Link>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/images/hamburgCurry.jpg"
+            alt="Hamburg Curry"
+            width={800}
+            height={450}
+            style={{ width: '70%', height: 'auto' }}
+            priority
+          />
         </div>
       </div>
-
-      <div className={styles.orderText}>
-        {others.map((location) => (
-          <div key={location.address} className={styles.location}>
-            <p className={styles.locationName}>{location.name}</p>
-            <Link href={location.orderLink}>Online order</Link>
-
-            {(location.name === "FREMONT" || location.name === "CUPERTINO") && (
-              <div className={styles.deliveryLinks}>
-                <Link className={styles.doorDash} href={location.doordashLink}>
-                  DoorDash
-                </Link>
-                <Link className={styles.uberEats} href={location.ubereatsLink}>
-                  UberEats
-                </Link>
-              </div>
-            )}
-
-            {(location.name === "SARATOGA AVE" ||
-              location.name === "DUBLIN") && (
-              <div className={styles.doorDashLinks}>
-                <Link className={styles.doorDash} href={location.doordashLink}>
-                  DoorDash
-                </Link>
-              </div>
-            )}
-
-            <p>{location.address}</p>
-            <div className={styles.mapContainer}>
-              <iframe
-                src={location.mapSrc}
-                frameBorder="0"
-                allowFullScreen
-                title={location.name}
-              ></iframe>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      <div className={styles.contact}>
-        Customer Support: Only text messages are available at{" "}
-        <strong>650-833-8775</strong>
-      </div>
-      <div className={styles.spacer}></div>
     </Layout>
   );
 }
